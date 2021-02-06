2acaebbbfa88a9497523862d1f8249c4901734c1
Aum <imaum2208@gmail.com>
2021-02-06 23:47:42 +0000

    fixed sword animation gets stuck

---
+++ Assets/Scripts/LizardMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LizardMove : MonoBehaviour {
+    public Transform main;
+
+    public float speed = 0.01f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        main.position = new Vector3(main.position.x, main.position.y, main.position.z+speed);
+    }
+}
